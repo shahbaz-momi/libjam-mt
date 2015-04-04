b910897f15b5f6e30eaf4ef06666b7cdb26d80f6
shahbaz-man <shahbaz.man3@gmail.com>
2015-04-04 15:29:50 -0400

    Type safety

--- src/com/asdev/libjam/mt/SmartTouch.java
+++ src/com/asdev/libjam/mt/SmartTouch.java
@@ -11,7 +11,7 @@ public class SmartTouch implements Runnable {
 	public void run() {
 		while(true){
 			
-			int execS = System.nanoTime();
+			long execS = System.nanoTime();
 			for(int i = 0; i < 10; i ++){
 				long val = TouchHandler.syncTTGet(i);
 				if(Math.abs(execS - val) >= TOUCH_TIMEOUT_NS){
